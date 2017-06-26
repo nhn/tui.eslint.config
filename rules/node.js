52d68065ca1cf30155dc8d27d9cfbb6f8870c5c4
youjung-hong <youjung.hong@nhnent.com>
2017-06-26 10:31:19 +0900

    chore: split rules by categories

---
+++ rules/node.js
@@ -0,0 +1,13 @@
+module.exports = {
+    rules: {
+        // Node.js and CommonJS
+        "global-require": 2,
+        "handle-callback-err": 2,
+        "no-mixed-requires": [2, true],
+        "no-new-require": 2,
+        "no-path-concat": 2,
+        "no-process-exit": 2,
+        "no-restricted-modules": 0,
+        "no-sync": 0
+    }
+}
