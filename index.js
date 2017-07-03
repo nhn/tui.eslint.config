7eb3581c0da063e169b22ca29ce7000449cbb167
youjung-hong <youjung.hong@nhnent.com>
2017-07-03 11:10:10 +0900

    Revert "fix: split ES6 rule from default entry file"
    This reverts commit 6db9307e3440d78354d89a0c3efa0c08664e0dd0.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+'use strict';
+
 module.exports = {
     extends: [
         './rules/best-practices',
         './rules/errors',
+        './rules/es6',
         './rules/node',
         './rules/strict',
         './rules/style',
         './rules/variables'
     ].map(require.resolve),
     env: {
-        'browser': true
+        "browser": true
     }
-};
+}
