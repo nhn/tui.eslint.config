60a71e07bb6e7cb80953abc701c16f3565ab8ed6
youjung-hong <youjung.hong@nhnent.com>
2017-07-03 11:14:15 +0900

    Revert "style: change double quotes to single quote"
    This reverts commit 9ce4a9f0b2599ce109d12f86ac2157e9e846b498.

--- rules/node.js
+++ rules/node.js
@@ -1,17 +1,17 @@
 module.exports = {
     env: {
-        'node': true,
-        'commonjs': true
+        "node": true,
+        "commonjs": true
     },
     rules: {
         // Node.js and CommonJS
-        'global-require': 2,
-        'handle-callback-err': 2,
-        'no-mixed-requires': [2, true],
-        'no-new-require': 2,
-        'no-path-concat': 2,
-        'no-process-exit': 2,
-        'no-restricted-modules': 0,
-        'no-sync': 0
+        "global-require": 2,
+        "handle-callback-err": 2,
+        "no-mixed-requires": [2, true],
+        "no-new-require": 2,
+        "no-path-concat": 2,
+        "no-process-exit": 2,
+        "no-restricted-modules": 0,
+        "no-sync": 0
     }
 }
